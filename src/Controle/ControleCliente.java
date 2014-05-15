f2f67e9d7d162111edf4195834af977d0b9e1b47
Rodolfo Kohei Soken <rodolfosoken@gmail.com>
2014-05-15 17:19:20 -0300

    Revert "DAO_cliente funcionando e rotina de cadastro do cliente implementada."

--- src/Controle/ControleCliente.java
+++ src/Controle/ControleCliente.java
@@ -6,10 +6,27 @@
 
 package Controle;
 import Model.Cliente;
+import java.util.HashMap;
 /**
  *
  * @author Rodolfo
  */
-public class ControleCliente {    
-    
+public abstract class  ControleCliente {    
+   public static boolean cadastraCliente(HashMap<String,String> dataCliente){
+       
+       Cliente cliente = new Cliente();
+       cliente.setNome(dataCliente.get("nome"));
+       cliente.setCodClient(Integer.parseInt(dataCliente.get("codigo")));
+       cliente.setApto(Integer.parseInt(dataCliente.get("apto")));
+       cliente.setBairro(dataCliente.get("bairro"));
+       cliente.setCEP(Integer.parseInt(dataCliente.get("cep")));
+       cliente.setCidade(dataCliente.get("cidade"));
+       cliente.setComplemento(dataCliente.get("complem"));
+       cliente.setRua(dataCliente.get("rua"));
+       cliente.setUf(dataCliente.get("uf"));
+       cliente.setCpf(Long.parseLong(dataCliente.get("cpf")));
+       cliente.setCEP(Integer.parseInt(dataCliente.get("cep")));
+             
+       return cliente.salvar();
+   }
 }
