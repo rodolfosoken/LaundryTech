6ae293d17b411ed6b80e53514d83e6213deeb58a
Rodolfo Kohei Soken <rodolfosoken@gmail.com>
2014-05-22 17:41:42 -0300

    JUnit : cadastra cliente GUI (Bot) 2

--- test/gui/CadastraClienteTest.java
+++ test/gui/CadastraClienteTest.java
@@ -67,16 +67,47 @@ public class CadastraClienteTest {
     public void testCadastraCliente() throws AWTException, InterruptedException {
         CadastraCliente win = new CadastraCliente();
         win.setVisible(true);
+
         Robot bot = new Robot();
-        bot.keyPress(KeyEvent.VK_4);
-        Thread.sleep(250);
-        bot.keyRelease(KeyEvent.VK_5);
-        Thread.sleep(250);
-        bot.keyPress(KeyEvent.VK_6);
-        Thread.sleep(250);
-        bot.keyRelease(KeyEvent.VK_6);
-        Thread.sleep(1000);
+        int t = 30; // tempo entre pressionar e soltar teclas
+
+        //digita telefone
+        int[] digita = {KeyEvent.VK_DELETE, KeyEvent.VK_DELETE, KeyEvent.VK_DELETE,
+            KeyEvent.VK_4, KeyEvent.VK_6,KeyEvent.VK_4, KeyEvent.VK_6,KeyEvent.VK_4, 
+            KeyEvent.VK_6,KeyEvent.VK_4, KeyEvent.VK_6};
+        for (int i : digita) {
+            bot.keyPress(i);
+            Thread.sleep(t);
+            bot.keyRelease(i);
+            Thread.sleep(t);
+        }
         
+        //digita nome
+        int[] digitaNome = {KeyEvent.VK_TAB, KeyEvent.VK_TAB,
+            KeyEvent.VK_DELETE, KeyEvent.VK_DELETE, KeyEvent.VK_DELETE, KeyEvent.VK_DELETE,
+            KeyEvent.VK_R, KeyEvent.VK_O, KeyEvent.VK_D, KeyEvent.VK_O, KeyEvent.VK_L, KeyEvent.VK_F, KeyEvent.VK_O};
+        for (int i : digitaNome) {
+            bot.keyPress(i);
+            Thread.sleep(t);
+            bot.keyRelease(i);
+            Thread.sleep(t);
+        }
+        
+        
+        //digita nome
+        int[] digitaRua = {KeyEvent.VK_TAB,
+            KeyEvent.VK_DELETE, KeyEvent.VK_DELETE, KeyEvent.VK_DELETE,
+            KeyEvent.VK_A, KeyEvent.VK_V, KeyEvent.VK_PERIOD, KeyEvent.VK_SPACE, KeyEvent.VK_E, 
+            KeyEvent.VK_S, KeyEvent.VK_T,KeyEvent.VK_A, KeyEvent.VK_D, KeyEvent.VK_O};
+        for (int i : digitaRua) {
+            bot.keyPress(i);
+            Thread.sleep(t);
+            bot.keyRelease(i);
+            Thread.sleep(t);
+        }
+
+        Thread.sleep(250);
+
         //clique de finalização
         bot.mouseMove(510, 350);
         bot.mousePress(InputEvent.BUTTON1_MASK);
@@ -86,17 +117,17 @@ public class CadastraClienteTest {
         bot.keyPress(KeyEvent.VK_ENTER);
         Thread.sleep(250);
         bot.keyRelease(KeyEvent.VK_ENTER);
-        
-        
+
         DAO_cliente dao = new DAO_cliente();
-        
+        int tel = 46464646;
+
         //consulta se o cliente foi cadastrado
-        boolean result = dao.consultaCliente(46123);
+        boolean result = dao.consultaCliente(tel);
         boolean resultExpected = true;
         assertEquals(resultExpected, result);
-        
+
         //exclui cliente cadastrado
-        boolean result2 = dao.excluiCliente(46123);
+        boolean result2 = dao.excluiCliente(tel);
         boolean resultExpected2 = false;
         assertEquals(resultExpected2, result2);
 
